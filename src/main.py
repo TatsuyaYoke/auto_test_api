bb30007f1223e455889bd010da125f5e581a01e2
Yoke <tayok@synspective.com>
2022-08-09 11:34:23 +0900

    CORS

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 import src.routers.bus
 
 app = FastAPI()
+
+origins = [
+  "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(src.routers.bus.router, prefix="/bus", tags=["bus"])
 
 
