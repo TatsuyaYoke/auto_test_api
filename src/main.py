bcc3c39a7772d18e939afda2764b52d2d89958ac
Yoke <tayok@synspective.com>
2022-08-15 13:12:58 +0900

    add routers for obs and trans

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 import src.routers.bus
+import src.routers.obs
+import src.routers.trans
 
 app = FastAPI()
 
@@ -20,6 +22,8 @@ app.add_middleware(
 )
 
 app.include_router(src.routers.bus.router, prefix="/bus", tags=["bus"])
+app.include_router(src.routers.obs.router, prefix="/obs", tags=["obs"])
+app.include_router(src.routers.trans.router, prefix="/trans", tags=["trans"])
 
 
 @app.get("/")
---
+++ src/routers/obs.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/hello")
+async def obs_hello() -> dict[str, str]:
+    return {"Hello": "obs"}
